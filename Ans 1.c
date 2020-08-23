f2c8531a3eb19b6d98c9021702f99b4737b469a4
Aditya Singh <65218559+aditya256bit@users.noreply.github.com>
2020-08-24 01:41:03 +0530

    Create Ans 1.c
    Ques 1.  WAP to accept a String from User and Display it Back on Screen

---
+++ Ans 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+
+   char str1[50];
+   
+   printf("\nEnter the String : ");
+   gets (str1);
+   
+   printf("\nThe entered String is : %s ", str1 );
+   
+   return 0;
+
+}
